f7cc9b0191fb39604cff52cd16d54f4a7845b920
Denis-Pankov <angelman777@mail.ru>
2023-01-06 08:54:23 +0300

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';

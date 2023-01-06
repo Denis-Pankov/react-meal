bf8106866921824cac3e95dda8dd2ac1e6e23b23
Denis-Pankov <angelman777@mail.ru>
2023-01-06 12:38:39 +0300

    Change HashRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
-import { Home } from './pages/Home';
 import { Category } from './pages/Category';
 import { NotFound } from './pages/NotFound';
 import { Recipe } from './pages/Recipe';
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 function About() {
-  return <h1>Hello? from About page</h1>;
+  return <h1>About page</h1>;
 }
 
 export { About };
